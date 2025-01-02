3a83999fc2ec7e152aec86d2a203299c8176a9fd
97-Yanjin <85831850+97-Yanjin@users.noreply.github.com>
2025-01-02 17:42:10 +0800

    update files

--- Funs/main_max.m
+++ Funs/main_max.m
@@ -1,88 +1,129 @@
-function [S,  obj] = main_max(A, X,k)
-%»ñÈ¡ÊÓÍ¼ÊýÄ¿
+function [result_our,obj,U,S0,S0_initial] = main_max( X,  c, lambda)
+
+for j=1:length(X)
+           colnum=size(X{j},2);           % colnum = 24
+           mole = repmat(std(X{j},0,2),1,colnum);
+           mole(mole==0) = 1; 
+           X{j}=(X{j}-repmat(mean(X{j},2),1,colnum))./mole;
+ end
+ k = 7;
+for v = 1 :length(X)
+    XX{v} = X{v}';
+end
+options.NeighborMode = 'KNN';
+options.k = k; 
+options.WeightMode = 'HeatKernel';
+for v = 1:length(X)
+    fea_v = XX{1,v};
+    A(v) = {constructW(fea_v',options)}; 
+end
+
+S0_initial = A;
+
+%èŽ·å–è§†å›¾æ•°ç›®
 m = length(X);
 
-%»ñÈ¡Ñù±¾Êý
+%èŽ·å–æ ·æœ¬æ•°
 n = size(A{1},1);
 
-%% init
-a = ones(1,m) / m;
+%% init W
+W = orth(rand(n, c));
 
-La = cell(m,1);
+%% init La
+La = cell(1,m);
 for i=1:m
     AA = (A{i}' + A{i}) / 2;
     DD = diag(sum(AA));
     La{i} = DD - AA;    
 end
+
+dw = zeros(1, m);
+
+A_rep = zeros(n);
+for v = 1:m
+   num = size(A{v},1);
+   S10 = (A{v}+A{v}')/2;
+   D10 = diag(sum(S10));
+   L0 = D10 - S10;
+   [F0, ~, ~] = eig1(full(L0), c+1, 0);
+   F{v} = F0(:,1:c);
+   A_sub{v} = F{v} * F{v}';
+   A_rep = A_rep + A{v};
+end
+
+K0 = zeros(num,num);  
+for v = 1:m   
+    K0 = K0 + A_sub{v};
+end
+
+
 niter = 20;
-lambda = 1;
 
 for it=1:niter    
-    fprintf('****ÕâÊÇµÚ%dÂÖ****\n',it)
-    
-    %¸üÐÂU
-    U = cell(m, 1);
-    for i=1:m
-        U{i} = (eye(n) + 2*La{i})^(-1)*X{i};    
+    fprintf('****è¿™æ˜¯ç¬¬%dè½®****\n',it)
+     
+    for v = 1:m   
+        dw(v) = 1/(2 * norm(W*W' - A{v}, 'fro'));
     end
     
-    %¸üÐÂS
-    A_hat = zeros(n,n);
-    for i =1:m
-       A_hat = A_hat + (a(i)*A{i})'*(a(i)*A{i});
-    end
-    S = eig1(A_hat,n,1);
-    
-    %¸üÐÂa
-    for i = 1 : m
-        beta(i) = -trace(S'*A{i}'*A{i}*S);
-    end
-    a = (beta) .^-1 / sum((beta) .^-1);
-    
-    %¸üÐÂA
-    for i=1:m
-        [A{i},belta(i)] = update_A(U{i}', a(i), lambda, S, k);
+    %æ›´æ–°A
+    WW = W*W';
+    idxx = cell(1,m);
+    ed = cell(1,m);
+    for v = 1:m
+        ed{v} = L2_distance_1(X{v}', X{v}');
+        [~, idxx{v}] = sort(ed{v}, 2); % sort each row
     end
     
-    La = cell(m,1);
+    for v = 1:m
+        A{v} = zeros(num);
+        for i = 1:num
+            id = idxx{v}(i,2:k+2);
+            di = ed{v}(i, id);
+            
+            numerator = di(k+1)-di+2*dw(v)*WW(i,id(:))-2*dw(v)*WW(i,id(k+1));
+            bta(i) = (k*di(k+1)-sum(di(1:k))-2*k*dw(v)*WW(i,id(k+1))-2*dw(v))/2;
+            denominator1 = k*di(k+1)-sum(di(1:k));
+            denominator2 = 2*dw(v)*sum(WW(i,id(1:k)))-2*k*dw(v)*WW(i,id(k+1));
+            A{v}(i,id) = max(numerator/(denominator1+denominator2+eps),0);
+        end
+         belta{v} = mean(bta);
+    end
+    
+    La = cell(1,m);
     for i=1:m
         AA = (A{i}' + A{i}) / 2;
         DD = diag(sum(AA));
         La{i} = DD - AA;    
     end
    
-    obj(it) = get_obj(X, U, S, a, A, belta);
-    lambda = obj(it);
-%     if it>2 && (obj(it) -obj(it-1)) / obj(it-1) < 1e-4
-%           break
+%     for v = 1:m   
+%         dw(v) = 1/(2 * norm(W*W' - A{v}, 'fro'));
 %     end
+   
+    %æ›´æ–°W
+    A0 = zeros(n, n);
+    for v = 1 : m
+        A0 = A0 + dw(v)*A{v};
+    end
+    A_temp = 2*A0 + lambda * K0;
+    [W, ~, ~] = eig1(A_temp, n, 1);
+    W = W(:,2:c+1);
+   
+        % Calculate Obj
+%     res = 0;
+%     for v = 1 : m
+%         res = res + norm(W*W' - A{v}, 'fro')^2+2*trace(X{v}'*La{v}*X{v})+ belta{v} *norm(A{v},'fro')^2;
+%     end
+%     obj(it) = res - lambda*trace(W'*K0*W);
+%     if it > 2 && abs(obj(it) - obj(it-1)) / abs(obj(it-1)) < 10^(-4)
+%         break
+%     end
+
 end
-S = S;
-obj = obj;
-end
-
-
-
-
-function obj = get_obj(X, U, S, a, A, belta)
-m = length(X);
-
-for i=1:m
-    AA = (A{i}' + A{i}) / 2;
-    DD = diag(sum(AA));
-    La{i} = DD - AA;    
-end
-
-numerator = 0;         %·Ö×Ó
-denominator = 0;     %·ÖÄ¸
-for i=1:m
-%     numerator = numerator + norm(S*(a(i)*A{i}),'fro')^(2);
-    numerator = numerator + a(i)*A{i};
-    
-    denominator = denominator + norm(X{i}-U{i},'fro')^(2) + 2*trace(U{i}'*La{i}*U{i}) + belta(i)*norm(A{i})^(2); 
-    
-end
-numerator = norm(S*numerator,'fro')^(2);
-obj = numerator / (denominator +eps);
-
+W = NormalizeFea(W,0);
+obj = 1;
+result_our = kmeans(W, c, 'emptyaction', 'singleton', 'replicates', 100, 'display', 'off');
+U = W*W';
+S0 = A;
 end
--- run_demo.m
+++ run_demo.m
@@ -6,24 +6,16 @@ addpath(genpath('./datasets'))
 addpath('./funs');
 addpath('./finchpp');
 addpath('./datasets');
-% 'ORL','MSRC','COIL20','Cite','Caltech101-7','Caltech101-20','bbcsport','3sources','MNIST.mat'
-data = {'ORL.mat'}; 
+data = {'MSRC.mat'}; 
 
-% load('ORL.mat')
-lambda = [0.1 0.3 0.5 0.7 0.9 1 1.1 1.3 1.5 1.7 1.9 2];
-% lambda = [1];
+
+% lambda = [0.1 0.3 0.5 0.7 0.9 1 1.1 1.3 1.5 1.7 1.9 2];
+lambda = [1];
 for idx = 1: length(data) 
     fprintf('-------%s\n',data{idx})
     load(data{idx})
-%     X = data;
-%     Y = truelabel{1};
-%     Y = truth;
-%     for i=1:length(X)
-%         X{i}=X{i}(1:1280,:);
-%     end
-%     Y = Y(1:1280,:);
-%     A = generate_graph(X, k);
-    c = length(unique(Y));   % 类别 classes = 7
+
+    c = length(unique(Y));   
 
 %     tic;
     for v = 1:length(lambda)
